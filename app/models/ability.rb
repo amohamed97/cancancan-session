92db198eae7b13a7509198e8f3eb6d3151024293
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2022-05-23 16:16:15 +0200

    STEP7: guests can list public posts

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,9 @@ class Ability
 
   def initialize(user)
     can :manage, Post, is_public: true
+
+    return unless user.present?
+
     can :manage, Post, is_approved: true
 
     return unless user.is_admin?
