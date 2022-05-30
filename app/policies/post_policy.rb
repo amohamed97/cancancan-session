0767724ae8d0ce6dba5b067ef9c3df020ec94f9a
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2022-05-30 14:40:07 +0200

    STEP5: only admin can access all posts

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,11 @@
 class PostPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(is_public: true)
+      if user.is_admin?
+        scope.all
+      else
+        scope.where(is_public: true)
+      end
     end
   end
 end
