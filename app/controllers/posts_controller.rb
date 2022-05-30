053cfddf70b17208127447c3c7eb5b6f068fa14e
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2022-05-30 16:31:42 +0200

    STEP14: use load_and_authorize_resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
   # before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: :index
-  load_resource
-  authorize_resource
+  load_and_authorize_resource
 
   # GET /posts or /posts.json
   def index
