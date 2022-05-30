849c0a8860cf533f2eac4e308a6bf750dbb453c5
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2022-05-30 14:47:16 +0200

    STEP7: guests can list public posts

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,6 +1,8 @@
 class PostPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
+      return scope.where(is_public: true) if user.nil?
+
       if user.is_admin?
         scope.all
       else
